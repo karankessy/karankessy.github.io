dd0634d57c7901d09b43fd043a1f0aad3c3661fd
karankessy <54462503+karankessy@users.noreply.github.com>
2026-03-23 05:22:00 +0000

    Deploying to gh-pages from @ karankessy/karankessy.github.io@d36a5ad9eb2f59cf2d2d25d3b01e941cb81f5057 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,6 +34,17 @@ ninja.data = [{
             window.location.href = "/blog/2026/understanding-cors/";
           
         },
+      },{id: "post-everything-is-inter-process-communication-http-rest-apis-serverless-functions-what-not",
+        
+          title: "Everything is Inter-Process Communication: HTTP, REST APIs, Serverless functions, what not",
+        
+        description: "Modern software is rarely a single process doing one thing. Browsers isolate tabs for security. Databases split query workers from storage workers. API backends offload CPU-heavy jobs to background processes. The moment you divide work across processes, you need them to talk to each other.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/everything-is-IPC/";
+          
+        },
       },{id: "post-f5-big-ip-wazuh-getting-logs-working-after-18-months",
         
           title: "F5 BIG-IP + Wazuh: Getting Logs Working After 18 Months",
